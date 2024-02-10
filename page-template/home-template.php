fba9fd0694866fb737fb1a9b1eb0c71c2db9b5fd
starmahmoodi <starmahmoodi@gmail.com>
2024-02-10 18:08:25 +0330

    44

--- page-template/home-template.php
+++ page-template/home-template.php
@@ -4,6 +4,88 @@ Template Name: Home
 */
 get_header();
 ?>
+<div class="mainHome mainView">
+    <div class="sectionOne">
+        <div class="sectionOneText">
+            <span>از رویا تا واقعیت</span>
+            <h1>سلام; ما <span class="h1Color">ایده چی</span> هستیم</h1>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و </p>
+            <a class="generalButton " href="#">شروع کنید <span><svg width="15" height="15" viewBox="0 0 1.05 1.05" xmlns="http://www.w3.org/2000/svg"><path d="M.791.817c.022 0 .037-.015.037-.04 0-.02-.011-.032-.029-.04L.326.526V.521L.799.313C.817.304.828.293.828.273A.037.037 0 0 0 .789.234a.064.064 0 0 0-.028.007L.267.468C.242.479.222.5.222.527c0 .028.02.047.045.059l.494.222a.07.07 0 0 0 .03.008Z"/></svg></span></a>
+            <a href="#">نمونه کارها <span><svg width="15" height="15" viewBox="0 0 1.05 1.05" xmlns="http://www.w3.org/2000/svg"><path d="M.791.817c.022 0 .037-.015.037-.04 0-.02-.011-.032-.029-.04L.326.526V.521L.799.313C.817.304.828.293.828.273A.037.037 0 0 0 .789.234a.064.064 0 0 0-.028.007L.267.468C.242.479.222.5.222.527c0 .028.02.047.045.059l.494.222a.07.07 0 0 0 .03.008Z"/></svg></span></a>
+        </div>
+        <div class="sectionOneImg">
+            <img src="<?php echo get_template_directory_uri() ?>/img/fff.png" alt="" width="100%" >
+        </div>
+    </div>
+    <div class="sectionTwo">
+        <div class="counterNumbersInner" id="counterNumbers">
+            <div class="counterItem aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000" data-aos-once="true">
+                <div class="counterNumber"><span id="counter-1">45</span></div>
+                <div class="counterTitle">سال تجربه</div>
+            </div>
+            <div class="counterItem aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000" data-aos-once="true">
+                <div class="counterNumber"><span id="counter-2">420</span><span>+</span></div>
+                <div class="counterTitle">پروژه ساختمانی</div>
+            </div>
+            <div class="counterItem aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000" data-aos-once="true">
+                <div class="counterNumber"><span id="counter-3">235</span><span>+</span></div>
+                <div class="counterTitle">نصب موفق VRF</div>
+            </div>
+            <div class="counterItem aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000" data-aos-once="true">
+                <div class="counterNumber"><span id="counter-4">1410</span><span>+</span></div>
+                <div class="counterTitle">رضایت مشتری</div>
+            </div>
+        </div>
+    </div>
+    <div class="sectionThree">
+        <div class="introSection">
+            <h2>خدمات ما</h2>
+            <img src="" alt="">
+            <p>ایده چی در چه زمینه هایی با شماست</p>
+        </div>
+        <div class="services">
+            <div class="service">
+                <img src="<?php echo get_template_directory_uri() ?>/img/fff.png" alt="" width="100%" >
+                <h3>گرافیک و هویت بصری</h3>
+                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و </p>
+            </div>
+            <div class="service">
+                <img src="<?php echo get_template_directory_uri() ?>/img/fff.png" alt="" width="100%" >
+                <h3>گرافیک و هویت بصری</h3>
+                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و </p>
+            </div>
+            <div class="service">
+                <img src="<?php echo get_template_directory_uri() ?>/img/fff.png" alt="" width="100%" >
+                <h3>گرافیک و هویت بصری</h3>
+                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و </p>
+            </div>
+            <div class="service">
+                <img src="<?php echo get_template_directory_uri() ?>/img/fff.png" alt="" width="100%" >
+                <h3>گرافیک و هویت بصری</h3>
+                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و </p>
+            </div>
+        </div>
+    </div>
+    <div class="sectionFour">
+        <div class="sectionOneText">
+            <span>درباره ایده چی</span>
+            <h1>هرآنچه برای خلق <span class="h1Color"> داستان </span> خود در دنیای دیجیتال نیاز دارید</h1>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و </p>
+            <a class="generalButton " href="#">بیشتر بخوانید <span><svg width="15" height="15" viewBox="0 0 1.05 1.05" xmlns="http://www.w3.org/2000/svg"><path d="M.791.817c.022 0 .037-.015.037-.04 0-.02-.011-.032-.029-.04L.326.526V.521L.799.313C.817.304.828.293.828.273A.037.037 0 0 0 .789.234a.064.064 0 0 0-.028.007L.267.468C.242.479.222.5.222.527c0 .028.02.047.045.059l.494.222a.07.07 0 0 0 .03.008Z"/></svg></span></a>
+            
+        </div>
+        <div class="sectionOneImg">
+            <img src="<?php echo get_template_directory_uri() ?>/img/fff.png" alt="" width="100%" >
+        </div>
+    </div>
+    <div class="sectionFive">
+    <div class="introSection">
+            <h2>نمونه کارها</h2>
+            <img src="" alt="">
+            <p>انواع وبسایت ها با طراحی اختصاصی</p>
+        </div>
+    </div>
+</div>
 
 
 
