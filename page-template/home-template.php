220a0d506af4794af2ce3f8134528e6bc977ebf7
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2024-02-12 11:22:57 +0330

    Update home-template.php

--- page-template/home-template.php
+++ page-template/home-template.php
@@ -89,6 +89,91 @@ get_header();
             <img class="lowRadius" src="<?php echo get_template_directory_uri() ?>/img/fff.png" alt="" >
         </div>
     </div>
+    <div class="sectionSix sectionPadding">
+        <div class="introSection">
+            <h2>پکیج های طراحی سایت</h2>
+            <img src="" alt="">
+            <p>راه اندازی وبسایت خود را به تخصص و تعهد ما بسپارید</p>
+        </div>
+        <div class="packagesWebsiteSecParent">
+            <div class="packageWebsiteOne packageWebsGeneral lowRadius">
+                <h3>وبسایت شرکتی</h3>
+                <span class="topPrices">شروع قیمت از</span>
+                <div class="packageWebPrice">
+                    <span class="price">5/5</span>
+                    <div class="priceSuffix">
+                        <span>میلیون</span>
+                        <span>تومان</span>
+                    </div>
+                </div>
+                <ul class="packageWebItems">
+                    <li>مناسب برای معرفی شرکت ها</li>
+                    <li>صفحات تخصصی معرفی خدمات</li>
+                    <li>پشتیبانی ۱۲ ماهه رایگان</li>
+                    <li>دارای بلاگ و صفحات مقالات</li>
+                    <li>آموزش کار با پنل رایگان</li>
+                </ul>
+                <a href="#" class="generalButton borderNoneButton linkButtonPackage">مشاهده جزئیات<span><svg width="15" height="15" viewBox="0 0 1.05 1.05" xmlns="http://www.w3.org/2000/svg"><path d="M.791.817c.022 0 .037-.015.037-.04 0-.02-.011-.032-.029-.04L.326.526V.521L.799.313C.817.304.828.293.828.273A.037.037 0 0 0 .789.234a.064.064 0 0 0-.028.007L.267.468C.242.479.222.5.222.527c0 .028.02.047.045.059l.494.222a.07.07 0 0 0 .03.008Z"></path></svg></span></a>
+            </div>
+            <div class="packageWebsiteTwo packageWebsGeneral lowRadius">
+                <h3>وبسایت فروشگاهی</h3>
+                <span class="topPrices">شروع قیمت از</span>
+                <div class="packageWebPrice">
+                    <span class="price">7/5</span>
+                    <div class="priceSuffix">
+                        <span>میلیون</span>
+                        <span>تومان</span>
+                    </div>
+                </div>
+                <ul class="packageWebItems">
+                    <li>مناسب برای معرفی شرکت ها</li>
+                    <li>صفحات تخصصی معرفی خدمات</li>
+                    <li>پشتیبانی ۱۲ ماهه رایگان</li>
+                    <li>دارای بلاگ و صفحات مقالات</li>
+                    <li>آموزش کار با پنل رایگان</li>
+                </ul>
+                <a href="#" class="generalButton borderNoneButton linkButtonPackage">مشاهده جزئیات<span><svg width="15" height="15" viewBox="0 0 1.05 1.05" xmlns="http://www.w3.org/2000/svg"><path d="M.791.817c.022 0 .037-.015.037-.04 0-.02-.011-.032-.029-.04L.326.526V.521L.799.313C.817.304.828.293.828.273A.037.037 0 0 0 .789.234a.064.064 0 0 0-.028.007L.267.468C.242.479.222.5.222.527c0 .028.02.047.045.059l.494.222a.07.07 0 0 0 .03.008Z"></path></svg></span></a>
+            </div>
+            <div class="packageWebsiteThree packageWebsGeneral lowRadius">
+                <h3>وبسایت پزشکی</h3>
+                <span class="topPrices">شروع قیمت از</span>
+                <div class="packageWebPrice">
+                    <span class="price">8</span>
+                    <div class="priceSuffix">
+                        <span>میلیون</span>
+                        <span>تومان</span>
+                    </div>
+                </div>
+                <ul class="packageWebItems">
+                    <li>مناسب برای معرفی شرکت ها</li>
+                    <li>صفحات تخصصی معرفی خدمات</li>
+                    <li>پشتیبانی ۱۲ ماهه رایگان</li>
+                    <li>دارای بلاگ و صفحات مقالات</li>
+                    <li>آموزش کار با پنل رایگان</li>
+                </ul>
+                <a href="#" class="generalButton borderNoneButton linkButtonPackage">مشاهده جزئیات<span><svg width="15" height="15" viewBox="0 0 1.05 1.05" xmlns="http://www.w3.org/2000/svg"><path d="M.791.817c.022 0 .037-.015.037-.04 0-.02-.011-.032-.029-.04L.326.526V.521L.799.313C.817.304.828.293.828.273A.037.037 0 0 0 .789.234a.064.064 0 0 0-.028.007L.267.468C.242.479.222.5.222.527c0 .028.02.047.045.059l.494.222a.07.07 0 0 0 .03.008Z"></path></svg></span></a>
+            </div>
+            <div class="packageWebsiteFour packageWebsGeneral lowRadius">
+                <h3>وبسایت خدماتی</h3>
+                <span class="topPrices">شروع قیمت از</span>
+                <div class="packageWebPrice">
+                    <span class="price">7</span>
+                    <div class="priceSuffix">
+                        <span>میلیون</span>
+                        <span>تومان</span>
+                    </div>
+                </div>
+                <ul class="packageWebItems">
+                    <li>مناسب برای معرفی شرکت ها</li>
+                    <li>صفحات تخصصی معرفی خدمات</li>
+                    <li>پشتیبانی ۱۲ ماهه رایگان</li>
+                    <li>دارای بلاگ و صفحات مقالات</li>
+                    <li>آموزش کار با پنل رایگان</li>
+                </ul>
+                <a href="#" class="generalButton borderNoneButton linkButtonPackage">مشاهده جزئیات<span><svg width="15" height="15" viewBox="0 0 1.05 1.05" xmlns="http://www.w3.org/2000/svg"><path d="M.791.817c.022 0 .037-.015.037-.04 0-.02-.011-.032-.029-.04L.326.526V.521L.799.313C.817.304.828.293.828.273A.037.037 0 0 0 .789.234a.064.064 0 0 0-.028.007L.267.468C.242.479.222.5.222.527c0 .028.02.047.045.059l.494.222a.07.07 0 0 0 .03.008Z"></path></svg></span></a>
+            </div>
+        </div>
+    </div>
     <div class="sectionSeven">
         <div class="sectionOneImg">
             <img src="<?php echo get_template_directory_uri() ?>/img/fff.png" alt="" width="100%" >
